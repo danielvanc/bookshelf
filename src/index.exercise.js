4a8240bc676df2032f0aee042aaceffc1ae168d3
Daniel Van Cuylenburg <email@danielvanc.com>
2021-05-24 20:33:49 +0100

    Exercise 1: Extra credit 2 - complete!

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Logo} from './components/logo'
+import { Dialog } from "@reach/dialog";
+import "@reach/dialog/styles.css";
 
 function App() {
+  const [showDialog, setShowDialog] = React.useState("none");
   return (
     <>
       <Logo width="80" height="80" />
@@ -11,16 +14,30 @@ function App() {
         <input
           type="button"
           value="Login"
-          onClick={() => alert('Clicked login')}
+          onClick={() => setShowDialog("Login")}
         />
       </div>
       <div>
         <input
           type="button"
           value="Register"
-          onClick={() => alert('Clicked register')}
+          onClick={() => setShowDialog("Register")}
         />
       </div>
+
+      <Dialog aria-label="Login form" isOpen={showDialog === "Login"}>
+        <div>
+          <button onClick={() => setShowDialog("none")}>Close</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+
+      <Dialog aria-label="Register form" isOpen={showDialog === "Register"}>
+        <div>
+          <button onClick={() => setShowDialog("none")}>Close</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
     </>
   )
 }
