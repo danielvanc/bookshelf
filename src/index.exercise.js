4e4a8011853a48867b3621ef037fd66fdfdebb25
Daniel Van Cuylenburg <email@danielvanc.com>
2021-05-25 20:59:17 +0100

    Exercise 1: Extra credit 3 - complete!

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -4,8 +4,44 @@ import {Logo} from './components/logo'
 import { Dialog } from "@reach/dialog";
 import "@reach/dialog/styles.css";
 
+function LoginForm({onSubmit, buttonText }) {
+
+  function handleSubmit(formData) {
+    formData.preventDefault()
+    const {username, password} = formData.target.elements;
+
+    onSubmit({
+      username: username.value,
+      password: password.value
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" id="username" />
+      </p>
+      <p>
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" id="password" />
+      </p>
+      <button type="submit">{buttonText}</button>
+    </form>
+  )
+}
+
 function App() {
   const [showDialog, setShowDialog] = React.useState("none");
+
+  function login(formData) {
+    console.log('login', formData);
+    
+  }
+  function register(formData) {
+    console.log('register', formData);
+  }
+
   return (
     <>
       <Logo width="80" height="80" />
@@ -30,6 +66,7 @@ function App() {
           <button onClick={() => setShowDialog("none")}>Close</button>
         </div>
         <h3>Login</h3>
+        <LoginForm onSubmit={login} buttonText={"Login"} />
       </Dialog>
 
       <Dialog aria-label="Register form" isOpen={showDialog === "Register"}>
@@ -37,6 +74,7 @@ function App() {
           <button onClick={() => setShowDialog("none")}>Close</button>
         </div>
         <h3>Register</h3>
+        <LoginForm onSubmit={register} buttonText={"Register"} />
       </Dialog>
     </>
   )
