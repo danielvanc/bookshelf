5a1e78d1098d0ac79442bd1ed2ff5b3ea7de3cbb
Daniel Van Cuylenburg <email@danielvanc.com>
2021-06-05 16:45:19 +0100

    Exercise 4: Extra credit 1 - complete!

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -2,42 +2,44 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from './utils/api-client'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+
+  if (token) {
+    const data = await client('me', { token })
+
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
-  
   const [user, setUser] = React.useState(null)
-  
-  function login(form) {
-    return auth.login(form).then(usr => setUser(usr));
-  }
 
-  function register(form) {
-    return auth.register(form).then(usr => setUser(usr))
-  }
-  
-  function logout() {
-    auth.logout();
+  const login = form => auth.login(form).then(u => setUser(u))
+  const register = form => auth.register(form).then(u => setUser(u))
+  const logout = () => {
+    auth.logout()
     setUser(null)
   }
+
+  React.useEffect(() => {
+    getUser().then(usr => setUser(usr))
+  }, [])
 
   return user ? (
     <AuthenticatedApp user={user} logout={logout} />
   ) : (
     <UnauthenticatedApp login={login} register={register} />
   )
-
-  // return <UnauthenticatedApp login={auth.login} register={auth.register} />
 }
 
-
-
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
