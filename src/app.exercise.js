d4b5c3ea68956c805339ab8f44d4b2beef2743c2
Daniel Van Cuylenburg <email@danielvanc.com>
2021-06-05 17:08:02 +0100

    Exercise 4: Extra credit 2 - complete!

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,18 +3,19 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import * as colors from './styles/colors'
+import {useAsync} from './utils/hooks'
+import {FullPageSpinner} from './components/lib'
+import {client} from './utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
-import {client} from './utils/api-client'
 
 async function getUser() {
   let user = null
 
   const token = await auth.getToken()
-
   if (token) {
-    const data = await client('me', { token })
-
+    const data = await client('me', {token})
     user = data.user
   }
 
@@ -22,24 +23,53 @@ async function getUser() {
 }
 
 function App() {
-  const [user, setUser] = React.useState(null)
-
-  const login = form => auth.login(form).then(u => setUser(u))
-  const register = form => auth.register(form).then(u => setUser(u))
-  const logout = () => {
-    auth.logout()
-    setUser(null)
-  }
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData,
+  } = useAsync()
 
   React.useEffect(() => {
-    getUser().then(usr => setUser(usr))
-  }, [])
+    run(getUser())
+  }, [run])
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) return <FullPageSpinner />
+  if (isError)
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}
