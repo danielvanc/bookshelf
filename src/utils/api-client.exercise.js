5ec5ad4c97000e83a78a2dacea11dda69ed0e60a
Daniel Van Cuylenburg <email@danielvanc.com>
2021-06-07 16:30:37 +0100

    Exercise 4: Extra credit 4 - complete!

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -3,10 +3,13 @@ const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {data, token, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
+      'Content-Type': data ? 'application/json' : undefined,
       Authorization: token ? `Bearer ${token}` : undefined,
       ...customHeaders,
     },
@@ -15,9 +18,10 @@ function client(
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     if (response.status === 401) {
-      await auth.logout();
+      await auth.logout()
+      // refresh the page for them
       window.location.assign(window.location)
-      return Promise.reject({ message: "Please re-authenticate!"})
+      return Promise.reject({message: 'Please re-authenticate.'})
     }
     const data = await response.json()
     if (response.ok) {
