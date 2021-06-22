ddedfc1d9c7d959e3b1ee792d0aeef8f31a9f632
Daniel Van Cuylenburg <email@danielvanc.com>
2021-06-22 08:36:11 +0100

    Exercise 8: Extra credit 2 - complete!

--- src/components/modal.exercise.js
+++ src/components/modal.exercise.js
@@ -2,17 +2,26 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-import {Dialog} from './lib'
+import VisuallyHidden from '@reach/visually-hidden'
+import {Dialog, CircleButton} from './lib'
+
+const callAll =
+  (...fns) =>
+  (...args) =>
+    fns.forEach(fn => fn && fn(...args))
 
 const ModalContext = React.createContext()
 
-function callAll(...fns) {
-  return (...args) => {
-    fns.forEach(fn => {
-      fn && fn(...args)
-    })
-  }
-}
+const circleDismissButton = (
+  <div css={{display: 'flex', justifyContent: 'flex-end'}}>
+    <ModalDismissButton>
+      <CircleButton>
+        <VisuallyHidden>Close</VisuallyHidden>
+        <span aria-hidden>×</span>
+      </CircleButton>
+    </ModalDismissButton>
+  </div>
+)
 
 function Modal(props) {
   const [isOpen, setIsOpen] = React.useState(false)
@@ -20,31 +29,41 @@ function Modal(props) {
   return <ModalContext.Provider value={[isOpen, setIsOpen]} {...props} />
 }
 
-function ModalDismissButton({onClick, children: child}) {
+function ModalDismissButton({children: child}) {
   const [, setIsOpen] = React.useContext(ModalContext)
   return React.cloneElement(child, {
     onClick: callAll(() => setIsOpen(false), child.props.onClick),
   })
 }
 
-function ModalOpenButton({onClick, children: child}) {
+function ModalOpenButton({children: child}) {
   const [, setIsOpen] = React.useContext(ModalContext)
   return React.cloneElement(child, {
     onClick: callAll(() => setIsOpen(true), child.props.onClick),
-    // onClick: (...args) => {
-    //   setIsOpen(true)
-    //   if (child.props.onClick) {
-    //     child.props.onClick(...args)
-    //   }
-    // },
   })
 }
 
-function ModalContents(props) {
+function ModalContentsBase(props) {
   const [isOpen, setIsOpen] = React.useContext(ModalContext)
   return (
     <Dialog isOpen={isOpen} onDismiss={() => setIsOpen(false)} {...props} />
   )
 }
 
-export {Modal, ModalDismissButton, ModalOpenButton, ModalContents}
+function ModalContents({title, children, ...props}) {
+  return (
+    <ModalContentsBase {...props}>
+      {circleDismissButton}
+      <h3 css={{textAlign: 'center', fontSize: '2em'}}>{title}</h3>
+      {children}
+    </ModalContentsBase>
+  )
+}
+
+export {
+  Modal,
+  ModalDismissButton,
+  ModalOpenButton,
+  ModalContents,
+  ModalContentsBase,
+}
