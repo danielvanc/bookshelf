4aa1eca2542acd61d114019405b422bcd5e32ad0
Daniel Van Cuylenburg <email@danielvanc.com>
2021-07-03 12:00:28 +0100

    Exercise 12: Solution 1 - complete!

--- src/components/__tests__/modal.exercise.js
+++ src/components/__tests__/modal.exercise.js
@@ -1,10 +1,39 @@
-// 🐨 you're gonna need this stuff:
-// import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import React from 'react'
+import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import {render, screen, within} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-test('can be opened and closed', () => {})
-// 🐨 render the Modal, ModalOpenButton, and ModalContents
-// 🐨 click the open button
-// 🐨 verify the modal contains the modal contents, title, and label
-// 🐨 click the close button
-// 🐨 verify the modal is no longer rendered
-// 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
+test('can be opened and closed', () => {
+  const label = 'Login form'
+  const title = 'Login'
+  const content = 'Modal contents'
+  render(
+    <Modal>
+      <ModalOpenButton>
+        <button>Login</button>
+      </ModalOpenButton>
+      <ModalContents aria-label={label} title={title}>
+        {content}
+      </ModalContents>
+    </Modal>,
+  )
+
+  const openButton = screen.getByRole('button', {name: /login/i})
+  userEvent.click(openButton)
+
+  const modal = screen.getByRole('dialog')
+  expect(modal).toHaveAttribute('aria-label', label)
+  // scope queries to just the dialog
+  const inModal = within(modal)
+  expect(inModal.getByRole('heading', {name: title})).toBeInTheDocument()
+  expect(inModal.getByText(content)).toBeInTheDocument()
+
+  // expect(queryByText(/modal contents/i))
+
+  // const closeButton = screen.getByRole('button', {name: /close/i})
+  const closeButton = inModal.getByRole('button', {name: /close/i})
+
+  userEvent.click(closeButton)
+
+  expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
+})
