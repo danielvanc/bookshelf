caa46ef0280189456984328dfc8d6582c5a35ac9
Daniel Van Cuylenburg <email@danielvanc.com>
2021-07-05 20:46:32 +0100

    Exercise 13: Extra credit 5 - 1 - complete!

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,15 +1,14 @@
 import * as React from 'react'
-
 import {
   render,
-  waitForLoadingToFinish,
   screen,
+  waitForLoadingToFinish,
   userEvent,
+  loginAsUser,
 } from 'test/app-test-utils'
-
-import {buildBook} from 'test/generate'
+import {buildBook, buildListItem} from 'test/generate'
 import * as booksDB from 'test/data/books'
-// import * as listItemsDB from 'test/data/list-items'
+import * as listItemsDB from 'test/data/list-items'
 import {formatDate} from 'utils/misc'
 import {App} from 'app'
 
@@ -76,3 +75,40 @@ test('can create a list item for the book', async () => {
   ).not.toBeInTheDocument()
   expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
 })
+
+test('can remove a list item for the book', async () => {
+  const user = await loginAsUser()
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  await listItemsDB.create(buildListItem({owner: user, book}))
+
+  await render(<App />, {route, user})
+
+  const removeFromListButton = screen.getByRole('button', {
+    name: /remove from list/i,
+  })
+
+  expect(removeFromListButton).toBeInTheDocument()
+  userEvent.click(removeFromListButton)
+  expect(removeFromListButton).toBeDisabled()
+
+  await waitForLoadingToFinish()
+
+  expect(
+    screen.getByRole('button', {
+      name: /add to list/i,
+    }),
+  ).toBeInTheDocument()
+
+  expect(
+    screen.queryByRole('button', {
+      name: /remove from list/i,
+    }),
+  ).not.toBeInTheDocument()
+
+  // screen.debug()
+})
+
+test('can mark a list item as read', () => {})
+
+test('can edit a note', () => {})
--- src/test/app-test-utils.exercise.js
+++ src/test/app-test-utils.exercise.js
@@ -5,9 +5,9 @@ import {
 } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {AppProviders} from 'context'
-import * as usersDB from 'test/data/users'
 import * as auth from 'auth-provider'
-import {buildUser} from 'test/generate'
+import {buildUser} from './generate'
+import * as usersDB from './data/users'
 
 async function render(ui, {route = '/list', user, ...renderOptions} = {}) {
   // if you want to render the app unauthenticated then pass "null" as the user
@@ -39,4 +39,4 @@ const waitForLoadingToFinish = () =>
   ])
 
 export * from '@testing-library/react'
-export {render, loginAsUser, waitForLoadingToFinish, userEvent}
+export {render, userEvent, loginAsUser, waitForLoadingToFinish}
